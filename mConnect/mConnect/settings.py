b68650c91757ef02d30c0aec668d0efba1e8269b
Jonathan Lo <jonathanlo411@gmail.com>
2021-09-05 15:22:49 -0700

    landing pages w/ routing + html/css

--- mConnect/mConnect/settings.py
+++ mConnect/mConnect/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'landing',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'mConnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mconnect',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
 
